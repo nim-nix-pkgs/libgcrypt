8d779207542a2eb3542ff6fa9b8459fd276192b7
hugosenari <hugosenari@gmail.com>
2022-03-12 02:55:25 -0300

    chore: re index libgcrypt

---
+++ 0_0_1/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''libgcrypt wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-libgcrypt-0_0_1.flake = false;
+  inputs.src-libgcrypt-0_0_1.owner = "FedericoCeratto";
+  inputs.src-libgcrypt-0_0_1.ref   = "refs/tags/0.0.1";
+  inputs.src-libgcrypt-0_0_1.repo  = "nim-libgcrypt";
+  inputs.src-libgcrypt-0_0_1.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-libgcrypt-0_0_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-libgcrypt-0_0_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = ''libgcrypt wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."libgcrypt-master".url = "path:./master";
+  inputs."libgcrypt-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."libgcrypt-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."libgcrypt-0_0_1".url = "path:./0_0_1";
+  inputs."libgcrypt-0_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."libgcrypt-0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."libgcrypt-0_1_1".url = "path:./0_1_1";
+  inputs."libgcrypt-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."libgcrypt-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
